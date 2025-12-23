8a6b60b6e539ffe2297b438d5c854f3ea1e0dc60
Riya <133260285+RiyaBhatta@users.noreply.github.com>
2025-12-23 19:05:26 +0530

    Update Signup.jsx

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -18,7 +18,7 @@ function Signup() {
           email: data.email,
           password: data.password
         }
-        await axios.post("https://ebook-backenddd.onrender.com/users/signup", userInfo)
+        await axios.post("https://ebook-back.onrender.com/users/signup", userInfo)
         .then((res)=>{
           console.log(res.data)
           if(res.data){
